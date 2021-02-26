127097edecb10b0e5c8b1e8d4db3f08813f38d18
picks <vinceghislain@gmail.com>
2021-02-26 07:54:20 +0100

    Added new component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="carousel">
+    <img v-bind:src="image.path" v-bind:alt="image.name" />
+    <p>{{ image.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  props: {
+    image: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.carousel {
+  border: 1px solid red;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
+  <div class="carousels">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Carousel v-for="image in images" :key="image.id" :image="image" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Carousel from '@/components/Carousel.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    Carousel
+  },
+  data() {
+    return {
+      images: [
+        {
+          id: 1,
+          name: 'Logo 1',
+          path: '../assets/logo.png'
+        },
+        {
+          id: 2,
+          name: 'Logo 2',
+          path: '../assets/logo.png'
+        },
+        {
+          id: 3,
+          name: 'Logo 3',
+          path: '../assets/logo.png'
+        }
+      ]
+    }
   }
-};
+}
 </script>
+
+<style scoped>
+.carousels {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
